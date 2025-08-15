cd0145a316360921ca432f87d2c519dc87780dbd
Álvaro Pardo Fente <alvaro.pardo.fente@udc.es>
2025-08-15 13:11:39 +0200

    Fix: adapted stats.py to the current csv headers

--- metrics/stats.py
+++ metrics/stats.py
@@ -7,6 +7,6 @@ pd.set_option("display.max_columns", None)
 
 df = pd.read_csv(sys.argv[1])
 
-median_df = df.groupby(["N", "Block size"]).median()
+median_df = df.groupby(["log2_N", "Block size"]).median()
 
-print(median_df.sort_values(["N", "Block size"]))
+print(median_df.sort_values(["log2_N", "Block size"]))
